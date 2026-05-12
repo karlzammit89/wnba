bfa8aac43318be20d909f29cda7980550e2e0c23
karlzammit89 <karlzammit89@gmail.com>
2026-05-12 03:04:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -381,8 +381,7 @@ if st.session_state.selected_game_id:
     away_id   = st.session_state.selected_away_id
     home_id   = st.session_state.selected_home_id
 
-    # Small columns to keep the buttons tight together
-    nav_col1, nav_col2, _ = st.columns([1, 2, 8])
+    nav_col1, nav_col2, _ = st.columns([1, 8, 8])
     
     with nav_col1:
         if st.button("⬅ Back to Schedule"):
